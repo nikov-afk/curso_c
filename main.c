e5735195875ff6061b0bbaee3e073572ca9eb83a
nikov-afk <kallash.nniikov@gmail.com>
2026-07-06 16:05:54 -0300

    aula 17

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /*
- * Operador sizeof
- *  aula 16
+Aula 17
+    operador short para o tipo int
+    intervalo: -32.768 até 32.767
+    %hi ou %d
  */
 
 int main(void) {
-    float x = 1.0;
-    printf("tamanho de um float na memoria: %d bytes\n", sizeof x);
-    printf("tamanho em memoria de um int: %d\n", sizeof(int));
+    int y = 0;
+    short int x = 32767;
+    printf("tamanho de um int na memoria: %d bytes\n", sizeof y);
+    printf("tamanho em memoria de um short int: %d\n", sizeof(x));
     return 0;
 }
