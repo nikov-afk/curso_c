8e3a700bbcca3ed5b289b871c4e92f104d571fd1
nikov-afk <kallash.nniikov@gmail.com>
2026-07-06 17:01:06 -0300

    aula 22

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 /*
-        Aula 21
-    tipo double
-    %lf
+        Aula 22
+    operador long para o tipo double
+    double -> %lf
+    long double -> %Lf
+
+    __mingw_printf();
 */
 
 int main(void) {
-    double x = 3.1415;
-    printf("tamanho de um double na memoria: %d bytes\n", sizeof(double));
-
-    printf("tamanho em memoria de um  double: %lf\n", x);
+    long double x = 3.14159848948498489412272;
+    printf("tamanho de um double na memoria: %d bytes\n", sizeof x);
+    __mingw_printf("tamanho em memoria de um long double: %.10Lf\n", x);
+    printf("tamanho em memoria de um long double: %.10Lf\n", x);
     return 0;
 }
