bf18c7d5f0235748ea8d20a56ccab934881d2ae8
nikov-afk <kallash.nniikov@gmail.com>
2026-07-09 15:36:51 -0300

    aula 131

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <time.h>
 
 /*
-    Aula 130: Lendo uma String com a função gets()
+    Aula 131: Lendo uma String com a função fgets()
 */
 
 int main() {
@@ -14,7 +14,8 @@ int main() {
 
     printf("Digite seu nome: ");
     //scanf("%10[^\n]",palavras);
-    gets(palavras);
+    //gets(palavras);
+    fgets(palavras, 10, stdin);
 
     printf("%s\n", palavras);
 
