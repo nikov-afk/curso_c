4e05dd8274f8f7e0cf1a2fb380f6ec9983eb3404
nikov-afk <kallash.nniikov@gmail.com>
2026-07-20 15:47:40 -0300

    aula 187

--- main.c
+++ main.c
@@ -5,55 +5,15 @@
 #include <string.h>
 #include <math.h>
 /*
-    Aula 173: Como criar uma função que retorna uma struct?
+    Aula 187: Ponteiro como parâmetro para uma função ou procedimento
 */
-typedef struct nascimento {
-    int dia, mes, ano;
-}Nascimento;
-
-typedef struct pessoa{
-    Nascimento data_nascimento;
-    int idade;
-    char sexo;
-    char nome[30];
-}Pessoa;
-
-void imprimir_pessoa(Pessoa pessoa) {
-    printf("Nome: %s \n",pessoa.nome);
-    printf("Sexo: %c\n",pessoa.sexo);
-    printf("Data de nascimento: %d/%d/%d \n",pessoa.data_nascimento.dia,pessoa.data_nascimento.mes
-        ,pessoa.data_nascimento.ano);
-    printf("Idade: %d",pessoa.idade);
+void imprimir(int *num) {
+    printf("%d\n", *num);
+    *num = 10;
 }
-
-Pessoa get_pessoa() {
-    Pessoa pessoa;
-    printf("Digite seu nome: \n");
-    fgets(pessoa.nome,30,stdin);
-
-    printf("Digite sua idade: \n");
-    scanf("%d",&pessoa.idade);
-    scanf("%c");
-
-    printf("Digite seu sexo (f, f, M, m) : \n");
-    scanf("%c",&pessoa.sexo);
-
-    printf("Digite sua data de nascimento no formato (dd mm aaaa): \n");
-    scanf("%d%d%d",&pessoa.data_nascimento.dia,&pessoa.data_nascimento.mes,&pessoa.data_nascimento.ano);
-    return pessoa;
-}
-
 int main() {
-    Pessoa pessoa1;
-    struct pessoa pessoa2;
-
-    pessoa2.idade = 20;
-    pessoa2.sexo = 'M';
-    strcpy(pessoa2.nome, "Snow");
-
-    imprimir_pessoa(pessoa2);
-    pessoa1 = get_pessoa();
-    imprimir_pessoa(pessoa1);
-
+    int idade = 35;
+    imprimir(&idade);
+    printf("\nidade = %d\n", idade);
     return 0;
 }
